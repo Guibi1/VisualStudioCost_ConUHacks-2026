c6b8ec95c3613d3b578a193baa8e06aee86a9deb
Laurent Stéphenne <laurent@guibi.dev>
2026-01-24 21:34:21 -0500

    Actual working test repo

---
+++ test-project/src/index.ts
@@ -0,0 +1,65 @@
+import { OpenRouter } from "@openrouter/sdk";
+import ai, { type ModelMessage } from "ai";
+
+async function greetUser() {
+    const openRouter = new OpenRouter();
+
+    const response = openRouter.chat.send({
+        model: "gemini-2.0-flash-001",
+        messages: [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: "prompt" },
+        ],
+    });
+    console.log(response);
+}
+
+async function compareResponses(prompt: string) {
+    const response_gpt_llm = await ai.generateText({
+        model: "openai/gpt-4.1",
+        prompt: [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: prompt },
+        ],
+    });
+    console.log(response_gpt_llm);
+
+    const response_gemini_llm = await ai.generateText({
+        model: "google/gemini-2.5-pro",
+        prompt: [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: prompt },
+        ],
+    });
+    console.log(response_gemini_llm);
+
+    // Do something here, like asking the user for feedback between the two responses!
+}
+
+async function agentMode(prompt: string) {
+    const messages: ModelMessage[] = [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: prompt },
+    ];
+
+    for (let i = 0; i < 10; i++) {
+        const response = await ai.generateText({
+            model: "google/gemini-2.5-pro",
+            prompt: messages,
+        });
+
+        messages.push(...response.response.messages);
+    }
+}
+
+function runExamples() {
+    greetUser();
+    compareResponses("Hello, how are you?");
+}
+
+function main() {
+    runExamples();
+    agentMode("Hi, can you help me set a timer?");
+}
+
+main();
